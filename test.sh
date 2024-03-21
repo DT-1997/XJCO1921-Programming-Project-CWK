6b65de96c576210ddc27d10cb31e942614e43932
DT-1997 <hx3026678597@163.com>
2024-03-21 20:15:48 +0800

    master

--- test.sh
+++ test.sh
@@ -8,15 +8,48 @@ echo -e "\n~~Argument Tests ~~"
 # the -n means 'print the next thing on the same line'
 # which makes it easier to read the output
 
+#Testing no arguments
 echo -n "Testing no arguments - "
-
 ./maze > tmp
-
-# And now I search to make sure the right print came out
-
-if grep -q "Usage: maze <filename>" tmp;
+if grep -q "Usage: maze <filename> <width> <height>" tmp;
 then
     echo "PASS"
 else
     echo "FAIL"
 fi
+
+#Testing 4 arguments
+echo -n "Testing 4 arguments - "
+./maze x x x x > tmp
+if grep -q "Usage: maze <filename> <width> <height>" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing the range of 2 arguments
+echo -n "Testing the range of 2 arguments - "
+width = 10
+height = 110
+./maze x $width $height > tmp
+if grep -q "The minimum value for width and height is 5 and the maximum value is 100";
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+
+echo -e "\n~~ File Handing~~"
+
+echo -n "Testing bad filename - "
+./maze fake.txt > tmp
+if grep -q "Error: Bad filename" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
